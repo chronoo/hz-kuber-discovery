f1a91862bc87bc74bad7c0deb2552d56b0906e1d
chrono <chrono059@gmail.com>
2023-04-28 00:10:09 +0300

    init

---
+++ src/main/java/com/example/hazelfallout/HazelFalloutApplication.java
@@ -0,0 +1,13 @@
+package com.example.hazelfallout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazelFalloutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HazelFalloutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hazelfallout/HazelFalloutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hazelfallout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazelFalloutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
